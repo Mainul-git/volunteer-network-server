c1156e5f452bc61f05287de76273ab9d9472a882
Md.Mainul Islam <u1902141@student.cuet.ac.bd>
2020-10-06 20:48:15 +0600

    volunteer-assignment

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ require('dotenv').config()
 
 
 
-const uri = `mongodb+srv://Mau:hungry@cluster0.wwsul.mongodb.net/volunteerAssign?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wwsul.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const app = express()
 
 
